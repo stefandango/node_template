984f9b582c294759c860ea5f70f2f28be94d6471
Stefan <stefanoep@gmail.com>
2019-10-21 16:26:11 +0200

    added example link / handlebar injection

--- controllers/about.js
+++ controllers/about.js
@@ -3,7 +3,7 @@ import express from 'express';
 let router = express.Router();
 
 router.get('/', (req, res) => {
-	res.render('about');
+	res.render('about', { index_link: '/' });
 });
 
 export default router;
--- controllers/index.js
+++ controllers/index.js
@@ -7,7 +7,7 @@ let router = express.Router();
 router.use('/about', aboutController);
 
 router.get('/', function(req, res) {
-	res.render('index');
+	res.render('index', { about_link: '/about' });
 });
 
 export default router;
