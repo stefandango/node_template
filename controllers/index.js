ba0706d1c6b9adb8e8436944b805ca67b3420e78
Stefan <stefanoep@gmail.com>
2019-10-21 16:44:47 +0200

    added simple error handling for express

--- controllers/index.js
+++ controllers/index.js
@@ -10,4 +10,13 @@ router.get('/', function(req, res) {
 	res.render('index', { about_link: '/about' });
 });
 
+router.get('*', function(req, res) {
+	res.send('404 - Page not found', 404); // TODO: Create pretty 404 page.
+});
+
+// eslint-disable-next-line no-unused-vars
+router.use(function(err, req, res, next) {
+	return res.status(500).send('Internal Error');
+});
+
 export default router;
